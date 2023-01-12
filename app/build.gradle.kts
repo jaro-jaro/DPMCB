7de32291065d4513ac47c80c48c4b5b491d6ed38
jaro-jaro <90213952+jaro-jaro@users.noreply.github.com>
2023-01-12 16:37:00 +0100

    Ci (#45)
    * Generated debug APK
    
    * Release + git fetch tags
    
    * v0.5.3.13
    
    * release
    
    Co-authored-by: jaro-jaro <jaro-jaro@users.noreply.github.com>
    Co-authored-by: rblaha15 <rblaha15@student.gymceska.cz>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "0.5.3.12"
+        versionName = "0.5.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
