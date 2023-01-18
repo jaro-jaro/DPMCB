d0bdbab67192a1f0d07f5847b54704d39a7acbc9
rblaha15 <rblaha15@student.gymceska.cz>
2023-01-18 17:57:11 +0100

    Lepší čas a trvání

--- app/src/main/java/cz/jaro/dpmcb/data/helperclasses/Trvani.kt
+++ app/src/main/java/cz/jaro/dpmcb/data/helperclasses/Trvani.kt
@@ -20,15 +20,6 @@ value class Trvani(val sek: Int) : Comparable<Trvani> {
 
         operator fun Int.times(o: Trvani) = times(o.sek).sek
         operator fun Double.times(o: Trvani) = times(o.sek).sek
-        operator fun Int.div(o: Trvani) = div(o.sek).sek
-        operator fun Double.div(o: Trvani) = div(o.sek).sek
-        operator fun Int.rem(o: Trvani) = rem(o.sek).sek
-        operator fun Double.rem(o: Trvani) = rem(o.sek).sek
-        operator fun Int.minus(o: Trvani) = minus(o.sek).sek
-        operator fun Double.minus(o: Trvani) = minus(o.sek).sek
-        operator fun Int.plus(o: Trvani) = plus(o.sek).sek
-        operator fun Double.plus(o: Trvani) = plus(o.sek).sek
-        fun Int.floorDiv(o: Trvani) = floorDiv(o.sek).sek
     }
 
     operator fun times(o: Int) = sek.times(o).sek
@@ -36,6 +27,7 @@ value class Trvani(val sek: Int) : Comparable<Trvani> {
     operator fun div(o: Trvani) = sek.toDouble().div(o.sek)
     operator fun rem(o: Trvani) = sek.rem(o.sek).sek
     operator fun plus(o: Trvani) = sek.plus(o.sek).sek
+    operator fun minus(o: Trvani) = sek.minus(o.sek).sek
 
     val min get() = sek / 60.0
     val hod get() = min / 60.0
