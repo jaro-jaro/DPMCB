0af69c20da81129fce7875b581ac889b3e3a0afe
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-01-22 11:29:59 +0100

    Nová verze: v0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "0.5.3.15"
+        versionName = "0.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
