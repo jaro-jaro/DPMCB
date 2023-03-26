86426141824e873172b6d967928723bea132e9b1
rblaha15 <rblaha15@student.gymceska.cz>
2023-03-26 13:29:52 +0200

    Verze 1.0-pre1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "0.5.6"
+        versionName = "1.0-pre1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
