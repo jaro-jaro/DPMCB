6d0cd321780f97f7cdfc1ab4661d041b389377c4
rblaha15 <rblaha15@student.gymceska.cz>
2023-03-26 11:37:34 +0200

    Super rychlý odjezdy
    Předtím:
    start: 73796822572000
    konec: 73800279825000
    rozdíl: 3457253000 ns (3.46 s)
    
    Potom:
    start: 41544086468000
    konec: 41544977215000
    rozdíl: 890747000 ns (0.89 s)
    
    Zrychleno o: 2566506000 ns (2.57 s)

--- app/src/main/java/cz/jaro/dpmcb/data/realtions/ZastavkaSpojeSeSpojem.kt
+++ app/src/main/java/cz/jaro/dpmcb/data/realtions/ZastavkaSpojeSeSpojem.kt
@@ -1,15 +1,17 @@
 package cz.jaro.dpmcb.data.realtions
 
 import cz.jaro.datum_cas.Cas
+import cz.jaro.datum_cas.Datum
 
-data class ZastavkaSpojeSeSpojemAJehoZastavkou(
+data class ZastavkaSpojeSeSpojem(
     val nazev: String,
     val cas: Cas,
     val indexZastavkyNaLince: Int,
     val cisloSpoje: Int,
     val linka: Int,
     val nizkopodlaznost: Boolean,
-    val jinaZastavkaSpojeNazev: String,
-    val jinaZastavkaSpojeCas: Cas,
     val pevneKody: String,
+    val jede: Boolean,
+    val od: Datum,
+    val `do`: Datum,
 )
