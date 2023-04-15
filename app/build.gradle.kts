0d8194649a8a8e978a581cb00dd824beaf816775
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-04-15 21:42:54 +0200

    CI + Verze 1.1.0-alpha.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.1.0-alpha.7"
+        versionName = "1.1.0-alpha.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
