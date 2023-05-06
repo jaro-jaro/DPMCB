328bbe0b5b5a6bc8dc3501740f47c70c8d6287b9
rblaha15 <rblaha15@student.gymceska.cz>
2023-05-06 16:34:19 +0200

    Verze 1.2.0-alpha.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.2.0-alpha.3"
+        versionName = "1.2.0-alpha.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
