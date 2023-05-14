417d3dbb4462a614c30014b41ecf5a5f52607c58
rblaha15 <rblaha15@student.gymceska.cz>
2023-05-14 18:37:27 +0200

    hm

--- app/src/main/java/cz/jaro/dpmcb/ui/theme/Theme.kt
+++ app/src/main/java/cz/jaro/dpmcb/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package cz.jaro.dpmcb.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightThemeColors = lightColorScheme(
 
@@ -85,6 +90,15 @@ fun DPMCBTheme(
         else -> when {
             useDarkTheme -> DarkThemeColors
             else -> LightThemeColors
+        }
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
         }
     }
 
