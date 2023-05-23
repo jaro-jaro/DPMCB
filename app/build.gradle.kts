f6464ca7ee6d8ca9218cbefc8c8e4a8be3109d49
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-05-23 07:42:32 +0200

    Verze 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.2.0-rc.6"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
