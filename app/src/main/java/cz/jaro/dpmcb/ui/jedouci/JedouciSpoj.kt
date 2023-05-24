863008acacba09c9b39dde27da2318af80fff4f1
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-05-24 16:55:24 +0200

    Jedoucí

--- app/src/main/java/cz/jaro/dpmcb/ui/jedouci/JedouciSpoj.kt
+++ app/src/main/java/cz/jaro/dpmcb/ui/jedouci/JedouciSpoj.kt
@@ -1,14 +1,10 @@
 package cz.jaro.dpmcb.ui.jedouci
 
-import cz.jaro.dpmcb.data.helperclasses.Smer
 import java.time.LocalTime
 
 data class JedouciSpoj(
-    val cisloLinky: Int,
     val spojId: String,
-    val cilovaZastavka: Pair<String, LocalTime>,
-    val pristiZastavka: Pair<String, LocalTime>,
+    val pristiZastavkaNazev: String,
+    val pristiZastavkaCas: LocalTime,
     val zpozdeni: Int,
-    val indexNaLince: Int,
-    val smer: Smer,
 )
