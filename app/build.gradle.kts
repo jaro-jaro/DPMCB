7985b39b61cc25b09b47b717191aeb0f2379335f
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-08-07 09:48:37 +0200

    AAAAAAAAAAA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
             manifestPlaceholders += "logo" to "@mipmap/logo_dpmcb"
