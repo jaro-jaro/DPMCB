aa3a2f222972ce40f50cd3c127ec8bbd4d461550
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2023-10-22 15:15:39 +0200

    Verze 1.4.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cz.jaro.dpmcb"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.4.0-RC.5"
+        versionCode = 3
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
