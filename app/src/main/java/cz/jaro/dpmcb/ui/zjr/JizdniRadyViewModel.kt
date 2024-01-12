d21984443758cdd9514e6a7fad6cc1c2fc1a6953
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-01-12 19:00:04 +0100

    #293

--- app/src/main/java/cz/jaro/dpmcb/ui/zjr/JizdniRadyViewModel.kt
+++ app/src/main/java/cz/jaro/dpmcb/ui/zjr/JizdniRadyViewModel.kt
@@ -2,9 +2,11 @@ package cz.jaro.dpmcb.ui.zjr
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import cz.jaro.dpmcb.data.DopravaRepository
 import cz.jaro.dpmcb.data.SpojeRepository
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.WhileSubscribed
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -15,6 +17,7 @@ import kotlin.time.Duration.Companion.seconds
 @KoinViewModel
 class JizdniRadyViewModel(
     private val repo: SpojeRepository,
+    private val dopravaRepo: DopravaRepository,
     @InjectedParam params: Parameters,
 ) : ViewModel() {
 
@@ -24,8 +27,18 @@ class JizdniRadyViewModel(
         val pristiZastavka: String,
     )
 
-    val state = repo.datum.map {
-        JizdniRadyState.Success(repo.zastavkyJedouciVDatumSPristiZastavkou(params.cisloLinky, params.zastavka, params.pristiZastavka, it))
+    val state = repo.datum.map { datum ->
+        JizdniRadyState.Success(
+            repo.zastavkyJedouciVDatumSPristiZastavkou(params.cisloLinky, params.zastavka, params.pristiZastavka, datum)
+        )
+    }.combine(dopravaRepo.seznamSpojuKterePraveJedou()) { jr, spojeNaMape ->
+        jr.copy(
+            data = jr.data.map { spoj ->
+                spoj.copy(
+                    zpozdeni = spojeNaMape.find { it.id == spoj.spojId }?.zpozdeniMin
+                )
+            }
+        )
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
@@ -34,7 +47,7 @@ class JizdniRadyViewModel(
 
     val zobrazitNizkopodlaznostZMinule = repo.zobrazitNizkopodlaznost
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5.seconds), false)
-    val nstaveni = repo.nastaveni
+
     fun upravitZobrazeniNizkopodlaznosti(value: Boolean) {
         viewModelScope.launch {
             repo.zmenitNizkopodlaznost(value)
