f67c0ccfbcdcae37a779a8aba1d32432493ac7a7
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-05-28 08:11:38 +0200

    levá již není pravá

--- app/src/main/java/cz/jaro/dpmcb/ui/common/icons/LeftHalfCircle.kt
+++ app/src/main/java/cz/jaro/dpmcb/ui/common/icons/LeftHalfCircle.kt
@@ -21,7 +21,7 @@ val Icons.Filled.LeftHalfCircle: ImageVector
             viewportHeight = 24F,
         ).materialPath {
             moveTo(x = 12F, y = 2F)
-            arcToRelative(a = 10F, b = 10F, theta = 180F, isMoreThanHalf = false, isPositiveArc = true, dx1 = 0F, dy1 = 20F)
+            arcToRelative(a = 10F, b = 10F, theta = 180F, isMoreThanHalf = false, isPositiveArc = false, dx1 = 0F, dy1 = 20F)
             close()
         }.build()
         return _leftHalfCircle!!
