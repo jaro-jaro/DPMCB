cf3338e87e3f913812d09ebf68f314545d5e8c86
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2024-09-26 20:16:07 +0200

    Verze 1.6.4 (51)
    This commit updates the app version to 1.6.4 and increments the version code to
     51. This indicates a stable release of version 1.6.4.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cz.jaro.dpmcb"
         minSdk = 26
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.6.4-RC.5"
+        versionCode = 51
+        versionName = "1.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
