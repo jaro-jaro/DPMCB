6bba3b215e0f8b7a452fad70a46665b8e488d141
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2025-03-07 23:23:21 +0100

    Refactor: Extract `AppUpdater` to an interface and create Android implementation
    This commit extracts the `AppUpdater` logic into an interface and creates an Android-specific implementation.
    - Created an `AppUpdater` interface.
    - Created `AndroidAppUpdater` class implementing the `AppUpdater` interface.
    - Moved `AppUpdater` implementation from `commonMain` to `androidMain`.
    - Removed now redundant `AppUpdater` class from `commonMain`.
    - Created `MyFileProvider` for providing files.
    - Moved `latestAppVersion` to the `androidMain` and made it non-member.
    - Moved `HttpURLConnection.use` to `androidMain` and made it non-member.
    - Renamed `BroadcastReceiver.kt` to put it under `androidMain`.

---
+++ composeApp/src/androidMain/kotlin/cz/jaro/dpmcb/ui/loading/AppUpdater.android.kt
@@ -0,0 +1,116 @@
+package cz.jaro.dpmcb.ui.loading
+
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import com.fleeksoft.ksoup.Ksoup
+import com.fleeksoft.ksoup.network.parseGetRequest
+import cz.jaro.dpmcb.data.recordException
+import io.ktor.utils.io.errors.IOException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.io.File
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class AndroidAppUpdater(
+    ctx: ComponentActivity,
+) : AppUpdater {
+    private val packageManager = ctx.packageManager
+    private val filesDir = ctx.filesDir
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    private val getUri: File.() -> Uri = {
+        FileProvider.getUriForFile(ctx, "${ctx.packageName}.provider", this)
+    }
+    private val startActivity: (Intent) -> Unit = {
+        ctx.startActivity(it)
+    }
+    private lateinit var callback: () -> Unit
+    private val launcher: ActivityResultLauncher<Intent> = ctx.registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { callback() }
+
+    private val intentToLaunch = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES).apply {
+        data = "package:${ctx.packageName}".toUri()
+    }
+
+    override fun updateApp(
+        loadingDialog: (String?) -> Unit,
+    ) {
+        callback = { updateApp(loadingDialog) }
+        if (!packageManager.canRequestPackageInstalls()) {
+            launcher.launch(intentToLaunch)
+            return
+        }
+        loadingDialog("Hledání nové verze…")
+
+        val apkDir = File(filesDir, "apk").apply {
+            if (!exists()) mkdir()
+        }
+
+        coroutineScope.launch(Dispatchers.Main) {
+            val latestVersion = latestAppVersion() ?: return@launch
+
+            val apkUrl =
+                "https://github.com/jaro-jaro/DPMCB/releases/download/v$latestVersion/Lepsi-DPMCB-v$latestVersion.apk"
+
+            val file = File(apkDir, "$latestVersion.apk")
+            file.createNewFile()
+
+            val connection = URL(apkUrl).openConnection() as HttpsURLConnection
+            loadingDialog("Stahování…")
+            connection.use { input ->
+                withContext(Dispatchers.IO) {
+                    file.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+                loadingDialog("Instalace…")
+
+                Intent(Intent.ACTION_VIEW).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    setDataAndType(file.getUri(), "application/vnd.android.package-archive")
+                }.let(startActivity)
+                loadingDialog(null)
+            }
+        }
+    }
+}
+
+suspend fun HttpURLConnection.use(block: suspend (BufferedInputStream) -> Unit) =
+    withContext(Dispatchers.IO) {
+        try {
+            connect()
+            inputStream.use {
+                withContext(Dispatchers.Main) {
+                    block(it.let(::BufferedInputStream))
+                }
+            }
+        } finally {
+            disconnect()
+        }
+    }
+
+class MyFileProvider : FileProvider()
+
+suspend fun latestAppVersion(): String? = withContext(Dispatchers.IO) {
+    val document = try {
+        Ksoup.parseGetRequest("https://raw.githubusercontent.com/jaro-jaro/DPMCB/main/app/version.txt")
+    } catch (e: IOException) {
+        e.printStackTrace()
+        recordException(e)
+        return@withContext null
+    }
+
+    return@withContext document.text()
+}
