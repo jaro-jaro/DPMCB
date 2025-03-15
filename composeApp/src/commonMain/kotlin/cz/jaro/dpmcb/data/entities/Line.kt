bab20021f9718f40cfc998cb3d46c85733f0a193
Radek Bláha <72446087+rblaha15@users.noreply.github.com>
2025-03-15 13:18:01 +0100

    Update Line.kt

--- composeApp/src/commonMain/kotlin/cz/jaro/dpmcb/data/entities/Line.kt
+++ composeApp/src/commonMain/kotlin/cz/jaro/dpmcb/data/entities/Line.kt
@@ -5,6 +5,7 @@ import cz.jaro.dpmcb.data.entities.types.LineType
 import cz.jaro.dpmcb.data.entities.types.VehicleType
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 import kotlin.jvm.JvmName
 
 @Entity("", [], false, primaryKeys = ["tab"], [], [])
@@ -15,13 +16,19 @@ data class Line(
 // Other
     val number: LongLine,
     val route: String,
+    @SerialName("vehicletype")
     val vehicleType: VehicleType,
+    @SerialName("linetype")
     val lineType: LineType,
+    @SerialName("hasrestriction")
     val hasRestriction: Boolean,
+    @SerialName("validfrom")
     val validFrom: LocalDate,
+    @SerialName("validto")
     val validTo: LocalDate,
 ) {
     @get:JvmName("getShortNumber")
+    @SerialName("shortnumber")
     var shortNumber = number.toShortLine()
         internal set
 }
