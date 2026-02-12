590db32c92f11820f7177a1c49b6d4c26a6460ef
rblaha15 <72446087+rblaha15@users.noreply.github.com>
2026-02-12 12:37:08 +0100

    appupdate

--- composeApp/src/commonMain/kotlin/cz/jaro/dpmcb/ui/loading/AppUpdater.kt
+++ composeApp/src/commonMain/kotlin/cz/jaro/dpmcb/ui/loading/AppUpdater.kt
@@ -1,9 +1,10 @@
 package cz.jaro.dpmcb.ui.loading
 
-//import com.fleeksoft.ksoup.Ksoup
-//import com.fleeksoft.ksoup.network.parseGetRequest
+import com.fleeksoft.ksoup.Ksoup
+import com.fleeksoft.ksoup.network.parseGetRequest
 import cz.jaro.dpmcb.data.helperclasses.IO
 import io.github.z4kn4fein.semver.Version
+import io.github.z4kn4fein.semver.toVersionOrNull
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -14,26 +15,26 @@ fun interface AppUpdater {
 }
 
 suspend fun latestAppVersion(): Version? = withContext(Dispatchers.IO) {
-//    val document = try {
-//        Ksoup.parseGetRequest("https://raw.githubusercontent.com/jaro-jaro/DPMCB/main/composeApp/version.txt")
-//    } catch (e: Exception) {
-//        e.printStackTrace()
+    val document = try {
+        Ksoup.parseGetRequest("https://raw.githubusercontent.com/jaro-jaro/DPMCB/main/composeApp/version.txt")
+    } catch (e: Exception) {
+        e.printStackTrace()
 //        recordException(e)
         return@withContext null
-//    }
+    }
 
-//    return@withContext document.text().toVersionOrNull(false)
+    return@withContext document.text().toVersionOrNull(false)
 }
 
 suspend fun latestAppPreReleaseVersion(currentVersion: Version): Version? = withContext(Dispatchers.IO) {
-//    val mmp = currentVersion.copy(preRelease = null, buildMetadata = null).toString()
-//    val document = try {
-//        Ksoup.parseGetRequest("https://raw.githubusercontent.com/jaro-jaro/DPMCB/refs/heads/release/v$mmp/composeApp/version.txt")
-//    } catch (e: Exception) {
-//        e.printStackTrace()
+    val mmp = currentVersion.copy(preRelease = null, buildMetadata = null).toString()
+    val document = try {
+        Ksoup.parseGetRequest("https://raw.githubusercontent.com/jaro-jaro/DPMCB/refs/heads/release/v$mmp/composeApp/version.txt")
+    } catch (e: Exception) {
+        e.printStackTrace()
 //        recordException(e)
         return@withContext null
-//    }
-//
-//    return@withContext document.text().toVersionOrNull(false)
+    }
+
+    return@withContext document.text().toVersionOrNull(false)
 }
